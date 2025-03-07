8c7446c21273d2f5c2070508d5fc218cfb4bc2f4
faukirijatul <faukirijatul42@gmail.com>
2025-03-07 12:09:06 +0700

    Task 6

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@
 // import Greeting from "./components/Greeting";
 // import JSXRules from "./components/JSXRules";
 // import ProductInfo from "./components/ProductInfo";
-import ProductList from "./components/ProductList";
-import UserList from "./components/UserList";
+import Person from "./components/Person";
+import Product from "./components/Product";
+// import ProductList from "./components/ProductList";
+// import UserList from "./components/UserList";
 // import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
     <section>
-      <UserList />
-      <ProductList />
+      <Person name="Fauki" age={28} />
+      <Product name="Laptop" price={1999} />
     </section>
   );
 };
