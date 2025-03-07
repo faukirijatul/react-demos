2613906020d2656dff67216a37ccb9d6a00284e4
faukirijatul <faukirijatul42@gmail.com>
2025-03-07 11:17:27 +0700

    Task 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 // import Add from "./components/Add";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 // import Greet from "./components/Greet";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent";
+import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
     <section>
-      <Header />
-      <MainContent />
-      <Footer />
+      <WelcomeMessage />
     </section>
   );
 };
