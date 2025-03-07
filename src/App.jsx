a1fe33486531ee0b61f0ec584c1d217c4ebca696
faukirijatul <faukirijatul42@gmail.com>
2025-03-07 11:26:45 +0700

    Task 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@
 // import Greet from "./components/Greet";
 // import Header from "./components/Header";
 // import MainContent from "./components/MainContent";
-import WelcomeMessage from "./components/WelcomeMessage";
+import JSXRules from "./components/JSXRules";
+// import WelcomeMessage from "./components/WelcomeMessage";
 
 const App = () => {
   return (
     <section>
-      <WelcomeMessage />
+      <JSXRules />
     </section>
   );
 };
---
+++ src/components/JSXRules.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX Rules</h1>
+      <p className="rules">JSX Rules</p>
+    </div>
+  );
+};
+
+export default JSXRules;
